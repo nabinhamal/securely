560bb9b62f6318fb0e279cc64aeb2a3b918c5ce7
nabinhamal <nabinhamal2@gmail.com>
2026-03-29 08:16:58 +0545

    feat: implement custom tab navigation layout, add global type definitions, and configure project assets and themes.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
 import "@/global.css";
 import { Link } from "expo-router";
+import { styled } from "nativewind";
 import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
---
+++ constants/data.ts
@@ -0,0 +1,24 @@
+import { icons } from "./icons";
+
+export const tabs: AppTab[] = [
+  {
+    name: "index",
+    title: "Home",
+    icon: icons.home,
+  },
+  {
+    name: "subscriptions",
+    title: "Subscriptions",
+    icon: icons.wallet,
+  },
+  {
+    name: "insignts",
+    title: "Insignts",
+    icon: icons.activity,
+  },
+  {
+    name: "settings",
+    title: "Settings",
+    icon: icons.setting,
+  },
+];
